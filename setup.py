fde6cee2b7bf6dd7d0f8f3d32f6a1be3cfe35b56
Steven Gong <gong.steven@hotmail.com>
2023-12-06 17:02:44 -0800

    Loosen quadprog requirement

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setuptools.setup(
     install_requires=[
         'numpy>=1.18.1',
         'scipy>=1.3.3',
-        'quadprog==0.1.7',
+        'quadprog>=0.1.7',
         'matplotlib>=3.3.1'
     ],
     classifiers=[
